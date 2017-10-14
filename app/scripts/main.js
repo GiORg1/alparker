d79183d04b16061540ec686c54096a4cf9489d80
Unknown <s.bien@jacobs-university.de>
2017-10-14 17:21:50 +0200

    Added submit button to set destination. Still need connection to the backend (POST method)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,7 +15,7 @@ function initAutocomplete() {
     mapTypeControl: true,
     mapTypeControlOptions: {
       style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-      position: google.maps.ControlPosition.TOP_CENTER
+      position: google.maps.ControlPosition.TOP_LEFT
     },
     zoomControl: true,
     zoomControlOptions: {
@@ -25,67 +25,67 @@ function initAutocomplete() {
 
 
   });
-
+  //Car Part done by Adam
   //code for creation and movement of cars
 
-  var cars = []; 
- 
-  var directionsService = new google.maps.DirectionsService(); 
+  var cars = [];
 
-  //Places a car at position 
-  function placeCar(position){ 
-    var car = new google.maps.Marker({ 
-      position: position, 
-      icon: './images/caricon.png', 
-      clickable: false, 
-      map: map 
-    }); 
-    car.path = []; 
-    car.startPosition = car.position; 
-    car.waypoints = []; 
-    car.totalDistance = 0; 
-    car.pointOnPath = 0; 
-    cars.push(car); 
+  var directionsService = new google.maps.DirectionsService();
 
-  } 
+  //Places a car at position
+  function placeCar(position){
+    var car = new google.maps.Marker({
+      position: position,
+      icon: './images/caricon.png',
+      clickable: false,
+      map: map
+    });
+    car.path = [];
+    car.startPosition = car.position;
+    car.waypoints = [];
+    car.totalDistance = 0;
+    car.pointOnPath = 0;
+    cars.push(car);
 
-  //Returns a random point within the area defined bottomLeft and topRight 
-  function randomPoint(bottomLeft, topRight){ 
-    var latDiff = topRight.lat() - bottomLeft.lat(); 
-    var lngDiff = topRight.lng() - bottomLeft.lng(); 
-    var newLat = bottomLeft.lat() + Math.random() * latDiff; 
-    var newLng = bottomLeft.lng() + Math.random() * lngDiff; 
-    return new google.maps.LatLng(newLat, newLng); 
-  } 
+  }
 
-  //Place cars at random position in latlng range and drive them to location 
-  var car_number = 1; 
+  //Returns a random point within the area defined bottomLeft and topRight
+  function randomPoint(bottomLeft, topRight){
+    var latDiff = topRight.lat() - bottomLeft.lat();
+    var lngDiff = topRight.lng() - bottomLeft.lng();
+    var newLat = bottomLeft.lat() + Math.random() * latDiff;
+    var newLng = bottomLeft.lng() + Math.random() * lngDiff;
+    return new google.maps.LatLng(newLat, newLng);
+  }
+
+  //Place cars at random position in latlng range and drive them to location
+  var car_number = 1;
   //this is our box we get data from - cars will be rendered inside and will also drive inside
   var limiting_coordinates = [new google.maps.LatLng(41.391603, 2.145333), new google.maps.LatLng(41.387482, 2.157242)]
-  for(var i = 0; i<car_number; i++){ 
-      var rPoint = randomPoint(limiting_coordinates[0], limiting_coordinates[1]); 
-      directionsService.route({ 
-      origin:rPoint, 
-      destination: randomPoint(limiting_coordinates[0], limiting_coordinates[1]), 
-      travelMode: google.maps.TravelMode.DRIVING 
-    }, function(result, status) { 
-      if (status == google.maps.DirectionsStatus.OK) { 
-        placeCar(new google.maps.LatLng(result.routes[0].legs[0].start_location.lat(), result.routes[0].legs[0].start_location.lng())); 
-      }else{ 
-        alert("Error: Google Maps API not returning location data. The app will restart."); 
-        window.location = "./"; 
-      } 
-    }); 
-  } 
+  for(var i = 0; i<car_number; i++){
+      var rPoint = randomPoint(limiting_coordinates[0], limiting_coordinates[1]);
+      directionsService.route({
+      origin:rPoint,
+      destination: randomPoint(limiting_coordinates[0], limiting_coordinates[1]),
+      travelMode: google.maps.TravelMode.DRIVING
+    }, function(result, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        placeCar(new google.maps.LatLng(result.routes[0].legs[0].start_location.lat(), result.routes[0].legs[0].start_location.lng()));
+      }else{
+        alert("Error: Google Maps API not returning location data. The app will restart.");
+        window.location = "./";
+      }
+    });
+  }
 
-  //Relocates car to position 
-  function moveCar(car, position){ 
-    car.setPosition(position); 
-  } 
+  //Relocates car to position
+  function moveCar(car, position){
+    car.setPosition(position);
+  }
 
-  //Moves a car along its path at the correct speed 
-  function advance(car){  
-    if(car.pointOnPath<car.path.length){ 
+  //Moves a car along its path at the correct speed
+  function advance(car){
+    if(car.pointOnPath<car.path.length){
       if(car.midpoint){
         if(car.pointOnPath<(car.path.length-1)){
           moveCar(car, new google.maps.LatLng(
@@ -97,37 +97,75 @@ function initAutocomplete() {
         car.pointOnPath++;
       }else{
         car.midpoint = true;
-        moveCar(car, car.path[car.pointOnPath]); 
+        moveCar(car, car.path[car.pointOnPath]);
       }
-      
+
     }else{
       car.midpoint = false;
 
       car.target = randomPoint(limiting_coordinates[0], limiting_coordinates[1]);
-      directionsService.route({ 
-          origin:car.getPosition(), 
-          destination:car.target, 
-          travelMode: google.maps.TravelMode.DRIVING 
-        }, function(result, status) { 
-          if (status == google.maps.DirectionsStatus.OK) { 
-            car.path = result.routes[0].overview_path; 
-            car.pointOnPath = 0; 
-          } 
-        } 
+      directionsService.route({
+          origin:car.getPosition(),
+          destination:car.target,
+          travelMode: google.maps.TravelMode.DRIVING
+        }, function(result, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+            car.path = result.routes[0].overview_path;
+            car.pointOnPath = 0;
+          }
+        }
       );
-    }         
-  } 
+    }
+  }
 
-  setTimeout(function() { 
-    timer = setInterval(function(){ 
-      for(var f = 0; f<car_number;f++){ 
-        advance(cars[f]); 
-      } 
-    }, 1500); 
-  }, 2000);   
+  setTimeout(function() {
+    timer = setInterval(function(){
+      for(var f = 0; f<car_number;f++){
+        advance(cars[f]);
+      }
+    }, 1500);
+  }, 2000);
+  //Car Part done by Adam
+
+  function CenterControl(controlDiv, map) {
+    // Set CSS for the button border.
+    var controlUI = document.createElement('div');
+    controlUI.style.backgroundColor = '#fff';
+    controlUI.style.border = '2px solid #fff';
+    controlUI.style.borderRadius = '3px';
+    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.marginBottom = '16px';
+    controlUI.style.textAlign = 'center';
+    controlUI.style.marginTop = '10px';
+    controlUI.title = 'Click to set the destination';
+    controlDiv.appendChild(controlUI);
+
+    // Set CSS for the button interior.
+    var controlText = document.createElement('div');
+    controlText.style.color = 'rgb(25,25,25)';
+    controlText.style.fontFamily = 'Comfortaa';
+    controlText.style.fontSize = '16px';
+    controlText.style.lineHeight = '28px';
+    controlText.style.paddingLeft = '5px';
+    controlText.style.paddingRight = '5px';
+    controlText.innerHTML = 'SUBMIT';
+    controlUI.appendChild(controlText);
+
+    // Setup the click event listeners: simply set the map to Chicago.
+    controlUI.addEventListener('click', function () {
+      console.log("Hello world!");
+      console.log(markers);
+      window.alert(markers[markers.length - 1].position);
+    });
+  };
+
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input); //comment this out to move the search box anywhere
+  var centerControlDiv = document.createElement('div');
+  var centerControl = new CenterControl(centerControlDiv, map);
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
 
   map.addListener('bounds_changed', function() {
     searchBox.setBounds(map.getBounds());
@@ -137,8 +175,7 @@ function initAutocomplete() {
   var markers = [];
   searchBox.addListener('places_changed', function() {
 
-    markers[0].setMap(null);
-    markers = [];
+    clearLocations();
     var places = searchBox.getPlaces();
 
 
@@ -148,9 +185,7 @@ function initAutocomplete() {
 
     // Clear out the old markers.
 
-    markers.forEach(function(marke0r) {
-      marker.setMap(null);
-    });
+    clearLocations();
 
     markers = [];
 
@@ -192,9 +227,7 @@ function initAutocomplete() {
   });
 
   map.addListener('click', function(event) {
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
+    clearLocations();
 
     placeMarker(event.latLng);
 
@@ -205,6 +238,15 @@ function initAutocomplete() {
       position: location,
       map: map,
     }));
+  }
+
+  function clearLocations() { //Clears out all marker data from array 'markers'
+    info_Window = new google.maps.InfoWindow();
+    info_Window.close();
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+    markers.length = 0;
   }
 
 }
