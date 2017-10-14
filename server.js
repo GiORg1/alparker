ccf7405132420a0e4fa9470eef95919989b6c013
Kanter666 <ak2g15@soton.ac.uk>
2017-10-14 22:54:14 +0200

    Finally got working version

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ var port = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/images'));
 
+// views is directory for all template files
+app.set('views', __dirname + '/views');
 // set the home page route
 app.get('/', function(req, res) {
 
