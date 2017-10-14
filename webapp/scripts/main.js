8de5adfc3acabeba9d6931033ca886ba9e1580aa
Kanter666 <ak2g15@soton.ac.uk>
2017-10-14 21:34:52 +0200

    heroku branch

--- webapp/scripts/main.js
+++ webapp/scripts/main.js
@@ -21,10 +21,36 @@ function initAutocomplete() {
     zoomControlOptions: {
       position: google.maps.ControlPosition.LEFT_CENTER
     },
-
-
-
   });
+
+  // Database code
+  const MongoClient = require('mongodb').MongoClient;
+
+  const MONGO_URL = 'mongodb://parker:parker@ds119685.mlab.com:19685/elparker';
+
+  MongoClient.connect(MONGO_URL, (err, db) => {  
+    if (err) {
+      return console.log(err);
+    }
+
+    // Do something with db here, like inserting a record
+    db.collection('real_cars').insertOne(
+      {
+        title: 'Hello MongoDB',
+        text: 'Hopefully this works!'
+      },
+      function (err, res) {
+        if (err) {
+          db.close();
+          return console.log(err);
+        }
+        // Success
+        db.close();
+      }
+    )
+  });
+
+
   //Car Part done by Adam
   //code for creation and movement of cars
 
